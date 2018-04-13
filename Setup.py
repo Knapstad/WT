6aade0bf95f337cdfcbc1a1ef7710e9e15d48ed7
Knapstad <b.knapsta@gmail.com>
2018-04-13 10:26:21 +0200

    rename

--- Setup.py
+++ Setup.py
@@ -5,15 +5,23 @@ Created on Fri Apr 13 08:08:34 2018
 @author: blk
 """
 
-from distutils.core import setup
+from setuptools  import setup, find_packages
 
-setup(name="WT",
-      version="1.0",
+setup(name="pywt",
+      version="1.0.1",
       description="Python Webtrends connector",
       author="Bendik Knapstad",
       author_email="post@pinligstillhet.no",
-      url=None,
-      packages=["requests", "typing", "json", "time", "os"],
-      data_files=[("seacrets/config.txt")])
+      url="https://github.com/Knapstad/WT",
+      py_modules=["wt"],
+      install_requires=["requests", "typing", "json", "time", "os"],
+      data_files=[("secrets/config.txt")],
+      classifiers=[ 
+      'Development Status :: 3 - Alpha',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3'],
+      packages=find_packages()
+
+    )
 
     
